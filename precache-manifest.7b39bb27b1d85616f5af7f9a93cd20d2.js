573deefa1fc8a8f36e1c8aa1933de6102782e9b0
Janis Dimants <dimants.janis99@gmail.com>
2020-01-09 15:02:44 +0200

    Updates

---
+++ precache-manifest.7b39bb27b1d85616f5af7f9a93cd20d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30b9f6ef14e5e731ffbace95dfee058",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d7aa91530ad103ecf316",
+    "url": "/monsters-rolodex/static/css/main.0ec29ab3.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7aa91530ad103ecf316",
+    "url": "/monsters-rolodex/static/js/main.0856b02e.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
